52aed767980fd797a97123187d3ce754c3cfdfe6
Nayani Paul <nayanipaul.24@gmail.com>
2025-07-24 00:43:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -164,7 +164,8 @@ async function createTransporter() {
       user: process.env.NOTIFY_EMAIL,
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      refreshToken: process.env.GOOGLE_REFRESH_TOKEN
+      refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
+      accessToken: token
     }
   });
 }
